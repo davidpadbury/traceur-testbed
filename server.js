f73d54c99e41bd351f509e3083c7bf8e782e434d
David Padbury <david@davidpadbury.com>
2011-08-05 19:46:57 -0400

    Initial commit of testbed

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http'),
+	static = require('node-static'),
+	file = new (static.Server)('./public');
+
+var server = http.createServer(function(req, res) {
+	req.addListener('end', function() {
+		file.serve(req, res);
+	});
+});
+
+server.listen(8081);
